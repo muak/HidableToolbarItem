c9f79c8c6748e1206089bf3d37633eb42f2d10f3
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2020-10-08 14:44:31 +0900

    delete unused code

--- Sample/CustomRenderers/MyNavigationPage.cs
+++ Sample/CustomRenderers/MyNavigationPage.cs
@@ -5,19 +5,6 @@ namespace Sample.CustomRenderers
 {
     public class MyNavigationPage:NavigationPage
     {		
-		/// <summary>
-		/// Android Only
-		/// </summary>
-		public static BindableProperty StatusBarBackColorProperty =
-			BindableProperty.Create(nameof(StatusBarBackColor), typeof(Xamarin.Forms.Color), typeof(MyNavigationPage), Xamarin.Forms.Color.Default,
-									defaultBindingMode: Xamarin.Forms.BindingMode.OneWay
-								   );
-
-		public Xamarin.Forms.Color StatusBarBackColor {
-			get { return (Xamarin.Forms.Color)GetValue(StatusBarBackColorProperty); }
-			set { SetValue(StatusBarBackColorProperty, value); }
-		}
-
 		public MyNavigationPage() { }
 		public MyNavigationPage(Page root) : base(root) { }
 	}
